e51b077edc71c9350e0e177b436f3186b775f585
asus_fausto <faustogiannottiur@gmail.com>
2025-04-24 16:12:39 -0300

    recien arrancando; defino y valido input del menu y defino input de tweet_a_almacenar, puede haber errores

--- algo_twitter.py
+++ algo_twitter.py
@@ -1,1 +1,51 @@
 # aca voy a hacer todo el desarollo
+
+NUMERO_INVALIDO = "Numero de tweet invalido."
+NO_ENCONTRADOS = "No se encontraron tweets."
+INPUT_INVALIDO = "Input invalido."
+FIN = "Finalizando..."
+RESULTADOS_BUSQUEDA = "Resultados de la busqueda:"
+TWEETS_ELIMINADOS = "Tweets eliminados:"
+ATRAS = "**"
+
+
+def main():
+    user_input = input(
+        "1. Crear Tweet \n2. Buscar Tweet \n3. Eliminar Tweet \n4. Salir\n"
+    )
+
+    if user_input == "1":
+        crear_tweet()
+    elif user_input == "2":
+        buscar_tweet()
+    elif user_input == "3":
+        eliminar_tweet()
+    elif user_input == "4":
+        return print(FIN)
+    else:
+        print("Seleccione una opción válida")
+
+# -----------------------------------------------------------------------------
+
+
+def crear_tweet():
+    tweet_a_almacenar = input("Ingrese el tweet a almacenar:")
+    return
+
+
+def buscar_tweet():
+    print("helo")
+
+
+def eliminar_tweet():
+    print("helo")
+
+
+# -----------------------------------------------------------------------------
+
+# Esta parte del código se ejecuta al final,
+# asegurando que se ejecute el programa
+# mediante la terminal correctamente y permitiendo que se puedan realizar
+# los tests de forma automática y aislada.
+if __name__ == "__main__":
+    main()
