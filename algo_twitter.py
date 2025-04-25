4e4fb1fc513b275c049a472c3fbfe30bed2cb845
asus_fausto <faustogiannottiur@gmail.com>
2025-04-25 01:23:42 -0300

    agrego documentacion, arranco crear_tweet() y mejoro main() para manejar errores, tamb lo convierto en un loop hasta que el user eliga salir (4)

--- algo_twitter.py
+++ algo_twitter.py
@@ -10,31 +10,66 @@ ATRAS = "**"
 
 
 def main():
-    user_input = input(
-        "1. Crear Tweet \n2. Buscar Tweet \n3. Eliminar Tweet \n4. Salir\n"
-    )
 
-    if user_input == "1":
-        crear_tweet()
-    elif user_input == "2":
-        buscar_tweet()
-    elif user_input == "3":
-        eliminar_tweet()
-    elif user_input == "4":
-        return print(FIN)
-    else:
-        print("Seleccione una opción válida")
+    while True:
+
+        # se ejecutará hasta que el usuario ingrese 4
+        user_input = input(
+            "1. Crear Tweet\n"
+            "2. Buscar Tweet\n"
+            "3. Eliminar Tweet\n"
+            "4. Salir\n"
+            ">>> "
+        )
+
+        if user_input == "1":
+            crear_tweet()
+        elif user_input == "2":
+            buscar_tweet()
+        elif user_input == "3":
+            eliminar_tweet()
+        elif user_input == "4":
+            return print(FIN)
+        else:
+            return print(INPUT_INVALIDO)
 
 # -----------------------------------------------------------------------------
 
+# todos los tweets se almacenan en un dict
+# cada tweet tiene un calve (id) y un valor (el tweet en si)
+# --> son de la forma {id:tweet}
+
+
+"""
+Crear tweet: el usuario ingresa un tweet que se almacena con su respectivo
+ID (que es unico e irrepetible)
+Output esperado: OK id_tweet
+Si tweet almacenado (i.e. normalizado) == vacío, entonces input invalido
+"""
+
 
 def crear_tweet():
+
     tweet_a_almacenar = input("Ingrese el tweet a almacenar:")
+
+    if tweet_a_almacenar == ATRAS:
+        return
+
     return
+
+
+"""
+Buscar tweet:
+"""
 
 
 def buscar_tweet():
     print("helo")
+
+
+"""
+Eliminar tweet:
+"""
 
 
 def eliminar_tweet():
